0319e3175f0f1c038edc2c7657165b05c6df92a0
tsubasa <mc0627tsubasa283@gmail.com>
2023-10-09 15:08:27 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,79 @@
+import pyautogui as pag
+import keyboard
+import sys
+
+# https://twitter.com
+
+SCREEN_SIZE = pag.size()
+
+rt_line = None
+more_line = None
+deleted_n = {"RT":0,"Tweet":0}
+
+def exit():
+    print(f"{deleted_n}")
+    sys.exit()
+
+print("RTボタンにポインタをおいてEnterを押してください。")
+while True:
+    if keyboard.is_pressed("esc"): exit()
+    if keyboard.is_pressed("enter"):
+        pos = pag.position()
+        pag.move(100,0)
+        pag.sleep(1)
+        rt_line = [pos.x-50,0,100+1,SCREEN_SIZE[1]]
+        if pag.locateOnScreen("imgs/RT.jpg",confidence=0.7,region=rt_line):
+            print("OK")
+            break
+        else:
+            print("Not found active RT button.")
+
+print("...ボタンにポインタをおいてEnterを押してください。")
+while True:
+    if keyboard.is_pressed("esc"): exit()
+    if keyboard.is_pressed("enter"):
+        pos = pag.position()
+        pag.move(100,0)
+        pag.sleep(1)
+        more_line = [pos.x-50,0,100+1,SCREEN_SIZE[1]]
+        if pag.locateOnScreen("imgs/more.jpg",confidence=0.7,region=more_line):
+            print("OK")
+            break
+        else:
+            print("Not found active more button.")
+
+def click(img_path,**args):
+    box = pag.locateOnScreen(img_path,**args)
+    if box:
+        pag.click(pag.center(box))
+        return True
+    else:
+        return False
+
+while True:
+    while True:
+        if keyboard.is_pressed("esc"): exit()
+        found = click("imgs/RT.jpg",region=rt_line,confidence=0.9,grayscale=False)
+        pag.sleep(1)
+        if not found:
+            break
+        pag.move(100,0)
+        pag.sleep(1)
+        click("imgs/RT-del.jpg",confidence=0.7)
+        pag.sleep(1)
+        deleted_n["RT"] += 1
+
+    if keyboard.is_pressed("esc"): exit()
+    found = click("imgs/more.jpg",region=more_line,confidence=0.7)
+    pag.move(100,0)
+    pag.sleep(1)
+    
+    if not click("imgs/del-1.jpg",confidence=0.7) or not found:
+        pag.scroll(-300)
+        pag.sleep(1.3)
+        continue
+
+    pag.sleep(1)
+    click("imgs/del-2.jpg",confidence=0.7)
+    pag.sleep(1)
+    deleted_n["Tweet"] += 1
